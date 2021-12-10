9142099f642f7ce597ddf32d9a29cfee16759286
theculturethinktank <94709781+theculturethinktank@users.noreply.github.com>
2021-12-10 16:06:52 -0600

    Update app.js

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ import {WelcomePage} from './pages/welcome/welcome';
 import {PropertyListPage} from './pages/property-list/property-list';
 import {BrokerListPage} from './pages/broker-list/broker-list';
 import {FavoriteListPage} from './pages/favorite-list/favorite-list';
+import {XxxListPage} from './pages/xxx-list/xxx-list';
 
 import {PropertyService} from './services/property-service';
 import {BrokerService} from './services/broker-service';
@@ -36,6 +37,7 @@ class MyApp {
             {title: 'Properties', component: PropertyListPage, icon: "home"},
             {title: 'Brokers', component: BrokerListPage, icon: "people"},
             {title: 'Favorites', component: FavoriteListPage, icon: "star"},
+            {title: 'XXX', component: XxxListPage, icon: "star"},
         ];
 
         this.rootPage = WelcomePage;
