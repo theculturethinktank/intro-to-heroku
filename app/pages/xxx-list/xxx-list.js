4eed021f495c88a5db3ea154ca172d82fb3268ea
theculturethinktank <94709781+theculturethinktank@users.noreply.github.com>
2021-12-10 15:31:38 -0600

    Create xxx-list.js

---
+++ app/pages/xxx-list/xxx-list.js
@@ -0,0 +1,30 @@
+import {OnInit} from '@angular/core';
+import {Page, NavController} from 'ionic-angular';
+import {XxxDetailsPage} from '../xxx-details/xxx-details';
+import {XxxService} from '../../services/xxx-service';
+
+@Page({
+    templateUrl: 'build/pages/xxx-list/xxx-list.html'
+})
+export class XxxListPage {
+
+    static get parameters() {
+        return [[NavController], [XxxService]];
+    }
+
+    constructor(nav, xxxService) {
+        this.nav = nav;
+        this.xxxService = xxxService;
+    }
+
+    ngOnInit() {
+        this.xxxService.findAll().subscribe(xxx => this.xxx = xxx);
+    }
+
+    itemTapped(event, xxx) {
+        this.nav.push(XxxDetailsPage, {
+            xxx: xxx
+        });
+    }
+
+}
