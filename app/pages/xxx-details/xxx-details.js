66254344f3abef92a3399beebed7d6a817a6ac14
theculturethinktank <94709781+theculturethinktank@users.noreply.github.com>
2021-12-11 09:44:11 -0600

    Update xxx-details.js

--- app/pages/xxx-details/xxx-details.js
+++ app/pages/xxx-details/xxx-details.js
@@ -1,1 +1,38 @@
+import {OnInit} from '@angular/core';
+import {Page, NavController, NavParams, Alert, ActionSheet} from 'ionic-angular';
+import {XxxService} from '../../services/xxx-service';
 
+
+@Page({
+    templateUrl: 'build/pages/xxx-details/xxx-details.html'
+})
+export class XxxDetailsPage {
+
+    static get parameters() {
+        return [[NavController], [NavParams], [XxxService]];
+    }
+
+    constructor(nav, navParams, xxxService) {
+        this.nav = nav;
+        this.xxxService = xxxService;
+        this.xxx = navParams.get('xxx');
+    }
+
+    ngOnInit() {
+        this.xxxService.findById(this.xxx.id).subscribe(xxx => this.xxx = xxx);
+    }
+
+    yyy(event, xxx) {
+
+        this.xxxService.yyy(xxx).subscribe(() => {
+            let alert = Alert.create({
+                title: 'YYY',
+                subTitle: 'XXX added to your YYY',
+                buttons: ['OK']
+            });
+            this.nav.present(alert);
+        });
+
+    }
+
+}
