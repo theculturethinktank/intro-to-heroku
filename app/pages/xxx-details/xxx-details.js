2859b72d23ff11aa2da68d3f7aaf8150b3fc56a2
theculturethinktank <94709781+theculturethinktank@users.noreply.github.com>
2021-12-13 15:51:34 -0600

    Update xxx-details.js

--- app/pages/xxx-details/xxx-details.js
+++ app/pages/xxx-details/xxx-details.js
@@ -38,10 +38,7 @@ export class XxxDetailsPage {
     
     submitOutput() {
     	console.log(this.inputName);
-    	
-        var headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        return this.http.post('/output', JSON.stringify({ 'output__c': this.inputName }), {headers: headers});
+    	this.xxxService.output(this.inputName).subscribe(input => this.inputName = input);
     }
 
 }
