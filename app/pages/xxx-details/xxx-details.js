c5fa88300e0bbd96308c209f7d3a810027c07ca8
theculturethinktank <94709781+theculturethinktank@users.noreply.github.com>
2021-12-14 15:06:35 -0600

    Update xxx-details.js

--- app/pages/xxx-details/xxx-details.js
+++ app/pages/xxx-details/xxx-details.js
@@ -38,8 +38,8 @@ export class XxxDetailsPage {
     
     submitOutput(full_name) {
         console.log(full_name);
-        let test = full_name;
-        console.log(test);
+        let testVar = full_name;
+        console.log(testVar);
         //console.log(this.inputName);
         //let outputText = this.inputName;
         //console.log(outputText);
