69176bcbc8aca20458626ab036cfb3fc70d3d4f6
theculturethinktank <94709781+theculturethinktank@users.noreply.github.com>
2021-12-14 15:08:48 -0600

    Update server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get('/yyy', function(req, res) {
 });
 
 app.post('/yyy', function(req, res) {
-  let randNumber= Math.floor(Math.random() * 10000000000);
+  let randNumber= testVar;
   client.query('INSERT INTO ' + yyyTable + ' (xxx__c, output__c) VALUES ($1, $2)', [req.body.xxx__c, randNumber], function(error, data) {
     res.json(data);
   });
