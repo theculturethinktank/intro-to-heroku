52f68e97e109a3c0f59f4da65b782b6f3743b616
theculturethinktank <94709781+theculturethinktank@users.noreply.github.com>
2021-12-15 13:31:15 -0800

    Update server.js

--- server.js
+++ server.js
@@ -26,9 +26,6 @@ var brokerTable = 'broker__c';
 var xxxTable = 'xxx__c';
 var yyyTable = 'yyy__c';
 
-// Define global variable
-global.testVar;
-global.testVar = 'aaa';
 
 // setup the demo data if needed
 client.query('SELECT * FROM salesforce.broker__c', function(error, data) {
