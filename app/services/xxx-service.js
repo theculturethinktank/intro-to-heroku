00b93e138025a2fc68beca370456dc3b7a54e502
theculturethinktank <94709781+theculturethinktank@users.noreply.github.com>
2021-12-15 12:50:07 -0800

    Update xxx-service.js

--- app/services/xxx-service.js
+++ app/services/xxx-service.js
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Http, Headers} from '@angular/http';
 import 'rxjs/Rx';
-import {onetwothree} from from '../../services/variable';
+import * as myGlobals from '../../services/variable';
 
 /*
     Prettify objects returned from Salesforce. This is optional, but it allows us to keep the templates independent
